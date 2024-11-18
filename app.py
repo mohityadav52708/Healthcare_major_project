9f546af1aca0b9307046d820a17d350c16f99b03
Mohit Yadav <rgmupdated@gmail.com>
2024-11-18 18:27:30 +0530

    new commit form mohit

--- app.py
+++ app.py
@@ -33,6 +33,9 @@ def contact():
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+@app.route('/booking')
+def booking():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
